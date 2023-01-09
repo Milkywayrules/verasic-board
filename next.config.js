1f0d54cae20ac9a3bcc9b4a5978a28a6dabf383f
Dio Ilham Djatiadi <apajabole1@gmail.com>
2023-01-09 11:35:09 +0700

    feat: use nextjs pageExt & add more rule to tsconf

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
+  pageExtensions: ['page.tsx', 'api.ts'],
 }
 
 module.exports = nextConfig
