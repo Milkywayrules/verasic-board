a12751910745f05621881382ba744f60374ed4a3
Dio Ilham Djatiadi <apajabole1@gmail.com>
2023-01-09 14:04:50 +0700

    chore(page/index): add state example

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,9 +1,14 @@
 import { Inter } from '@next/font/google'
 import Head from 'next/head'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [run, runSet] = useState(false)
+
+  const handle = () => runSet(s => !s)
+
   return (
     <>
       <Head>
@@ -16,6 +21,16 @@ export default function Home() {
       <main className={inter.className}>
         <h1 className="text-2xl font-black text-blue-800">Title here</h1>
         <p className="text-lg font-medium text-blue-600">Description here</p>
+
+        <button
+          type="button"
+          onClick={handle}
+          className="rounded bg-indigo-400 px-2 py-1 text-gray-100"
+        >
+          toggle
+        </button>
+
+        <p>{run ? 'run' : 'walk'}</p>
       </main>
     </>
   )
