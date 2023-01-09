87949fcc112fa34db5947e5551b84378d6fd2cfc
Dio Ilham Djatiadi <apajabole1@gmail.com>
2023-01-09 22:41:53 +0700

    chore(page): add default site title on _app

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,7 +1,17 @@
 import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Verasic Board · Board to ease your team daily work routine</title>
+      </Head>
+
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </>
+  )
 }
