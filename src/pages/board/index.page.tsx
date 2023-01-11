7b4c9d5b4b17c656de3cbe72913c94511bedc9e0
Dio Ilham Djatiadi <apajabole1@gmail.com>
2023-01-11 13:00:48 +0700

    chore(page/board): rm
    switch to `/dashboard/board`

--- src/pages/board/index.page.tsx
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface Props {}
-
-// eslint-disable-next-line no-empty-pattern
-export default function IndexBoardPage({}: Props) {
-  return 'HALO'
-}
