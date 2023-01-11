c888a488ef2a8ae0aabe73673b26ca3fda457ba9
Dio Ilham Djatiadi <apajabole1@gmail.com>
2023-01-11 12:34:44 +0700

    feat(.hierarchy): upd type structure

--- .hierarchy.ts
+++ .hierarchy.ts
@@ -1,26 +1,35 @@
-type ColumnName = 'To-do' | 'Ongoing' | 'Done'
+export type CardType = 'bug' | 'task' | 'story'
+type ColumnName =
+  | 'Product Backlog'
+  | 'Sprint Backlog'
+  | 'To-do'
+  | 'Ongoing'
+  | 'QA'
+  | 'Ready to deploy'
+  | 'Done'
+
 type MetaType = 'tes-board' | 'tes-whiteboard'
 
 interface Card {
-  id: '3ebc26e2bf21-card'
-  code: 'ASL-001'
-  title: 'Refactor page beli'
-  description: 'Refactor biar lebih cakep aja pokoknya mah'
-  story_point: 5
-  type: 'task'
-  reporter: 'Joko Widodo'
-  assignee: 'Dio Ilham'
+  id: '3ebc26e2bf21-card' | string
+  code: 'ASL-001' | string
+  title: 'Refactor page beli' | string
+  description: 'Refactor biar lebih cakep aja pokoknya mah' | string
+  story_point: '5' | string
+  type: CardType
+  reporter: 'Joko Widodo' | string
+  assignee: 'Dio Ilham' | string
 }
 
 interface Column {
-  id: '3ebc26e2bf11-column'
+  id: '3ebc26e2bf11-column' | string
   name: ColumnName
   cards: Card[]
 }
 
-interface Board {
-  id: '3ebc26e2bf11-board'
-  name: 'ASL Marketplace'
+export interface Board {
+  id: '3ebc26e2bf11-board' | string
+  name: 'ASL Marketplace' | string
   columns: Column[]
 }
 
